55c340187e1950c5b369f02284692bd8b3944133
HoangThong37 <thonghoang115@gmail.com>
2025-02-15 22:54:01 +0700

    update

--- homework/src/_02_Variable_Data_Type/Exercise4_IO_File.java
+++ homework/src/_02_Variable_Data_Type/Exercise4_IO_File.java
@@ -6,22 +6,21 @@ import java.util.Scanner;
 
 public class Exercise4_IO_File {
     public static void main(String[] args) {
-        try {
-            File file = new File(".../data/inputDate.txt"); // Open file
-            Scanner scanner = new Scanner(file);
+        File file = new File(".../data/inputDate.txt");
 
-            // Đọc dữ liệu từ file
+        if (!file.exists()) {
+            System.out.println("Error: File does not exist!");
+            return;
+        }
+
+        try (Scanner scanner = new Scanner(file)) { // Dùng try-with-resources
             int day = scanner.nextInt();
             int month = scanner.nextInt();
             int year = scanner.nextInt();
 
-            // In ra theo dinh dang dd/MM/yyyy
             System.out.printf("Dinh dang dd/MM/yyyy: %02d/%02d/%d\n", day, month, year);
-
-            scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error: File not found!");
-            e.printStackTrace();
         }
     }
 }
